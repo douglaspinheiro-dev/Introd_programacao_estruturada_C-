e05e639520be354d4e00410381d6454365be64b3
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2020-05-09 21:31:35 -0300

    fatorial concluido

---
+++ TrabalhoNotaAv1-05-2020/3-fatorial/index.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    int numero, resultado, i;
+    resultado = 1;
+    bool continuar;
+
+    do {
+        cout << "******************************************" << endl;
+        cout << "***********  Calculo de fatorial  ********" << endl;
+        cout << "******************************************" << endl;
+        
+        cout << "Insira a o numero a ser calculado:" << endl;
+        cin >> numero;
+
+        if ((numero != 0) || ( numero != 1)) { 
+            for (i = numero; i != 0; i--) {
+                resultado = resultado * i;
+            }
+        }
+
+        cout << "o fatorial e: " << resultado << endl;
+
+        // resetando a variavel pra continuar o loop
+        resultado = 1;
+        
+        cout << "Deseja continuar?" << endl;
+        cout << "1 - SIM" << endl;
+        cout << "0 - NAO" << endl;
+        cin >> continuar;
+
+    } while (continuar == 1);
+
+	system("pause");
+    return 0;
+}
